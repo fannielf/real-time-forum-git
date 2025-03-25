209f9a963c9173fd4a46a7b3f5f39d423a80cbaf
Maris Karu <maris.karu@gritlab.ax>
2025-03-25 10:07:18 +0200

    signup

--- js/app.js
+++ js/app.js
@@ -72,8 +72,10 @@ function loadPage() {
         loadPostPage();
     } else if (segments[0] === 'login') {
         page = 'login-page'
+        
     } else if (segments[0] === 'signup') {
         page = 'signup-page'
+        renderSignupPage();
     }
 
     showPage(page)
